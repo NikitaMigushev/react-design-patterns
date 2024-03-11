73b2c6a2e9a986c2c77b05b0b865ce0dddd2cc23
Nikita Migushev <migushev@gmail.com>
2024-03-11 10:38:19 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import {SplitScreen} from "./components/split-screen";
+
+const LeftSideComp = () => {
+    return (
+        <h2 style={{backgroundColor: 'crimson'}}>I am left!</h2>
+    )
+}
+
+const RightSideComp = () => {
+    return (
+        <h2 style={{backgroundColor: 'burlywood'}}>I am right!</h2>
+    )
+}
+
+function App() {
+  return (
+      <SplitScreen Left={LeftSideComp} Right={RightSideComp} />
+
+  )
+}
+
+export default App
